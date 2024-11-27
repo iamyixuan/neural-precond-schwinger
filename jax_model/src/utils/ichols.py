d8aa5c9af6940fdecfe8d1ad5a95a3e9321430c2
Srinivas Eswar <seswar@anl.gov>
2024-11-27 15:09:05 -0600

    Including ichol factorizations.

---
+++ jax_model/src/utils/ichols.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def quick_ichol0(A):
+'''
+Compute a quick ichol0 of a dense SPD matrix A.
+Input(s):
+  A  - SPD dense matrix (np.array)
+Output(s):
+  L0 - Dense lower triangular matrix (np.array)
+'''
+  Ad = A.todense()
+  L  = np.linalg.cholesky(Ad)
+  L0 = np.multiply(Ad != 0.0, L)
+  return L0
+
+def ichol0(A):
+'''
+Compute a ichol0 of a dense SPD matrix A.
+Input(s):
+  A  - SPD dense matrix (np.array)
+Output(s):
+  L0 - Dense lower triangular matrix (np.array)
+'''
+  n  = A.shape[0]
+  Ad = A.todense()
+  L0 = Ad.copy()
+  
+  for k in range(n):
+    L0[k, k] = np.sqrt(L0[k, k])
+
+    # Update along the column
+    for i in range(k+1, n):
+      if (L0[i, k] != 0.0):
+        L0[i, k] = L0[i, k] / L0[k, k]
+
+    # Update across the row
+    for j in range(k+1, n):
+      for i in range(j, n):
+        if (L0[i, j] != 0.0):
+          L0[i, j] = L0[i, j] - L0[i, k] * L0[j, k].conj()
+
+  L0 = np.tril(L0)
+  return L0
+
